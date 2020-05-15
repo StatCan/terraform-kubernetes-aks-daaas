773700520fa76f202efdc7af9d0f9e556e3da276
Zachary Seguin <zachary@zacharyseguin.ca>
2020-05-15 12:34:25 -0400

    feat(aks): Remove original cluster

--- virtual_network.tf
+++ virtual_network.tf
@@ -1,19 +1,4 @@
 # Virtual Networks
-
-resource "azurerm_virtual_network" "vnet_aks" {
-  name                = "${var.prefix}-vnet-aks"
-  location            = "${azurerm_resource_group.rg_network_development.location}"
-  resource_group_name = "${azurerm_resource_group.rg_network_development.name}"
-  address_space       = ["${var.vnet_cidr}"]
-}
-
-resource "azurerm_subnet" "subnet_aks" {
-  name                 = "containers"
-  resource_group_name  = "${azurerm_resource_group.rg_network_development.name}"
-  address_prefix       = "${var.subnet_cidr}"
-  virtual_network_name = "${azurerm_virtual_network.vnet_aks.name}"
-}
-
 resource "azurerm_virtual_network" "new_vnet_aks" {
   name                = "${var.new_prefix}-vnet"
   location            = "${azurerm_resource_group.rg_network_development.location}"
