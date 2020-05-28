0be6b845caf8af7d5066c92ff7fb81f20fa30e00
Zachary Seguin <zachary.seguin@canada.ca>
2020-05-28 12:01:07 -0400

    feat(acr): Upgrade to premium sku to enable tokens
    To allow for fine-grained access to the ACR registries

--- acr.tf
+++ acr.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "${var.short_prefix}acr"
   resource_group_name = "${azurerm_resource_group.rg_aks.name}"
   location            = "${azurerm_resource_group.rg_aks.location}"
-  sku                 = "Basic"
+  sku                 = "Premium"
   admin_enabled       = true
   # georeplication_locations = ["${var.georeplication_region}"]
 
